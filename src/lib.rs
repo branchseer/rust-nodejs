12edc07fa0a56c5c5bf2064aa193eb66bc6bbc08
patr0nus <dk4rest@gmail.com>
2021-07-29 12:39:44 +0800

    Update rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! Embedding Node.js in Rust.
+//!
+//! - Provide a global thread-safe Node.js channel.
+//! - Interact with the Node.js runtime via [Neon](https://neon-bindings.com) API.
+//! - Link with [prebuilt Node.js binaries](https://github.com/patr0nus/libnode/) to save compile time.
+//! - Native modules are supported.
+//!
+//! ## Guide
+//!
+//! 1. Copy this repo's ".cargo" folder to your cargo project to enable flags required to link Node.js properly.
+//! 2. `let channel = nodejs::channel()` to get the global Node.js channel.
+//! 3. Call `channel.send` to run tasks in the Node.js event queue.
+//! 4. Inside the task, use `nodejs::neon` for interoperability between Node.js and Rust. [Neon documentation](https://docs.rs/neon/0.9.0/neon/index.html)
+//!
+//! ## Example
+//! ```rust
+//! use nodejs::neon::{context::Context, reflect::eval, types::JsNumber};
+//!
+//! fn main() {
+//!     let (tx, rx) = std::sync::mpsc::sync_channel::<i64>(0);
+//!     let channel = nodejs::channel();
+//!     channel.send(move |mut cx| {
+//!         let script = cx.string("require('os').freemem()");
+//!         let free_mem = eval(&mut cx, script)?;
+//!         let free_mem = free_mem.downcast_or_throw::<JsNumber, _>(&mut cx)?;
+//!         tx.send(free_mem.value(&mut cx) as i64).unwrap();
+//!         Ok(())
+//!     });
+//!     let free_mem = rx.recv().unwrap();
+//!     println!("Free system memory: {}", free_mem);
+//! }
+//! ```
+
 mod sys;
 
 pub use neon;
