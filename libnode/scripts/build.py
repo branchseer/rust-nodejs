f78960568bf1ce0a0189e6a65b2d8f5caf750225
patr0nus <dk4rest@gmail.com>
2022-01-05 00:22:02 +0800

    Build darwin arm64 target

--- libnode/scripts/build.py
+++ libnode/scripts/build.py
@@ -16,6 +16,16 @@ if sys.platform == 'win32':
         ['cmd', '/c', 'vcbuild.bat', config.arch],
     )
 else:
+    if sys.platform == 'darwin' and config.arch != 'x64':
+        os.environ.update({
+            "GYP_CROSSCOMPILE": '1',
+            "CC": "cc -arch " + config.arch,
+            "CXX": "c++ -arch " + config.arch,
+            "CC_target": "cc -arch " + config.arch,
+            "CXX_target": "c++ -arch " + config.arch,
+            "CC_host": "cc -arch x86_64",
+            "CXX_host": "c++ -arch x86_64"
+        })
     configureArgvs += ["--dest-cpu=" + config.arch]
     subprocess.check_call([sys.executable, 'configure.py'] + configureArgvs)
     subprocess.check_call(['make', '-j4'])
