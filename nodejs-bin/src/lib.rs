6ae4bcf0cf2efe43f17cd6ced27b404456007d4f
patr0nus <dk4rest@gmail.com>
2022-01-07 00:30:58 +0800

    [wip] nodejs-bin

--- nodejs-bin/src/lib.rs
+++ nodejs-bin/src/lib.rs
@@ -1,10 +1,18 @@
 use std::os::raw::c_char;
 use std::path::PathBuf;
 
+#[cfg(not(target_os = "macos"))]
+fn res_path() -> PathBuf {
+    std::env::current_exe().unwrap().parent().unwrap().to_path_buf()
+}
+
 #[cfg(target_os = "macos")]
 fn res_path() -> PathBuf {
     use objc::{runtime::Object, *};
     use std::ffi::{CStr, CString};
+    use std::os::unix::ffi::OsStringExt;
+    use std::ffi::OsString;
+
     let resource_path = rc::autoreleasepool(|| unsafe {
         let ns_bundle_cls = class!(NSBundle);
         let main_bundle: *mut Object = msg_send![ns_bundle_cls, mainBundle];
@@ -12,9 +20,12 @@ fn res_path() -> PathBuf {
         let resource_path: *const c_char = msg_send![resource_path, UTF8String];
         CStr::from_ptr(resource_path).to_bytes().to_vec()
     });
-    panic!("dwq")
+    let resource_path = PathBuf::from(OsString::from_vec(resource_path));
+    resource_path
 }
 
 pub unsafe fn main() {
-    let _ = res_path();
+    let resource_path = res_path();
+    let embedder_path_file = resource_path.join("node_embedder");
+    println!("{:?}", embedder_path_file)
 }
